317411c14c48ff75b378b9dde23c4bd05a75d7f4
sukhoi <sakyraproto@gmail.com>
2024-07-27 01:46:21 -0500

    initalcommit

--- namechanger.lua
+++ namechanger.lua
@@ -1,1 +1,57 @@
+names = {
+    "I'm a major",
+    "You fw my bedbugs?",
+    "System health degrading..",
+    "Making pissed",
+    "babyeater2000",
+    "Pyramids under my skin",
+    "Oh my god",
+    "Training your left hand",
+    "I'm just a humbull guy",
+    "Femboys of the Phalanx",
+    "Shut the up...",
+    "If you die you're dead.",
+    "Babaubabau.",
+    "Whats yo angle..",
+    "What the frick is presision alignment!",
+    "Rain World is fucking stupid.",
+    "I know who rhat iss!",
+    "Tymbleflup",
+    "wat weird looking thing.",
+    "Put the cat ears on...",
+    "Warcrimes?",
+    "Meower..",
+    "WHY ARE RUSSIANS SO GOOD AT PVP",
+    "what the freak man..",
+    "over the hills and far away..",
+    "ok james charles",
+    "Mr Blobby",
+    "Youve lost bubby privileges",
+    "thats not frank sinatra",
+    "I can abuse harder than you KEKW",
+    "Dude that's lime gross and stuff",
+    "Uploaded images_30.jpg",
+    "Im actually 64 HU tall.",
+    "Can i propkill here..",
+    "If you do NG shit i will KILL YOU",
+    "Im into it.",
+    "I would not be any.",
+    "Im krilling you",
+    "Your nuh uh is invalid",
+    "Evil tiger",
+    "Not a dragon",
+    "spoinkbub get lolpop :)",
+    "its outraygeous!"
+    "scre u spunkbab"
+    "its kinda conchfusing"
+    "big shoe lmaoo!"
+    "RUSH!"
+    "egg.."
+    "Go shag a rock"
+}
 
+RunConsoleCommand("hostname",">SBK<" "Sukhoi's B/K ACF3" ..names[math.random(1,table.Count(names))]..)
+
+timer.Create("namechangr", 5, 0, function() 
+    RunConsoleCommand("hostname",">SBK<" "Sukhoi's B/K ACF3" ..names[math.random(1,table.Count(names))]..) )
+end)
